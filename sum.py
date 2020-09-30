13fb9f28aa443bdb0d2e841444ed693eaa05a628
KumarPrince246 <kumar8193prince@gmail.com>
2020-10-01 01:33:30 +0530

    added exception code

--- sum.py
+++ sum.py
@@ -10,5 +10,10 @@ def mul(a,b):
 def sub(a,b):
     s=a-b
     return(s)
-print("sum:", sum(a,b),"multiple:", mul(a,b),"subtraction", sub(a,b))
-
+def divide(a,b):
+    try:
+        d=a/b
+        return (d)
+    except ZeroDivisionError as e:
+        print(e," you are dividing by 0, not good")
+print("sum:", sum(a,b),"multiple:", mul(a,b),"subtraction:", sub(a,b), "Quotient:",divide(a,b))
